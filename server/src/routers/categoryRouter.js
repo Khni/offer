a983e3af4fe2006f3903c3cde2032d7b58bf63a0
khaledeveloper <khaledeskandrany@gmail.com>
2020-06-30 15:31:21 +0200

    category with products and sections

--- server/src/routers/categoryRouter.js
+++ server/src/routers/categoryRouter.js
@@ -3,6 +3,7 @@ const Category= require('../models/Category')
 const Section= require('../models/Section')
 const router = new express.Router()
 const auth = require('../middleware/adminAuth')
+const Product= require('../models/Product')
 
 
 
@@ -23,34 +24,69 @@ router.post('/category/add', auth, async (req, res) => {
 router.get('/category/findone/:id', auth, async (req, res) => {
     let foundCategory = await Category.findOne({_id: req.params.id})
     let sections = await Section.find({})
-   // let CategoryWithSections =  foundCategory
-  //  CategoryWithSections.sectionsOfCategory.sectionOfCategory = "dd"
+   
     let CategoryWithSections =  {...foundCategory.toObject(),sectionsOfCategory: await Promise.all( foundCategory.sectionsOfCategory.map(async(SOC)=>{
       return  await Section.findOne({_id: SOC.sectionOfCategory })
         // return await sections.filter((section) => section._id === SOC.sectionOfCategory)
     }) )   }
-   /* let CategoryWithSections = foundCategory.sectionsOfCategory.map((categorySection) =>{
-        return {...categorySection, sectionsOfCategory: sectionsOfCategory.map(async(section)=>{
-            return await Section.find({_id: section._id})
-        })}
-    })
-    
-    
-    , sectionOfCategory :foundCategory.sectionsOfCategory.map(async(categorySection)=>{
-        return await sections.filter((s)=>{
-        return s._id === categorySection.sectionOfCategory
-    })
-        //    return (await sections).filter
-        // return await Section.find({_id: SectionOfCategory._id})
-     }) 
-    */
+
 
 
     try {
-       // await category.save()
+ 
         res.status(201).send(CategoryWithSections)
     } catch (e) {
         res.status(400).send(e)
     }
 })
+
+router.get('/categories', auth, async (req, res) => {
+    let Categories = await Category.find({})
+  
+   
+    let CategoriesWithSectionsAndProducts =  await Promise.all(Categories.map((category)=> {
+           return {...category.toObject(), sectionsOfCategory: category.sectionsOfCategory.map((SOC)=>{
+    let sections=  await Section.findOne({_id: SOC.sectionOfCategory })
+           return {...sections, productsOfSection: sections.productsOfSection.map((POS)=>{
+                    return await Product.findOne({_id: POS.productOfSection })
+               })
+             })
+} )) 
+
+
+    try {
+ 
+        res.status(201).send(CategoriesWithSectionsAndProducts)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+
+
+
+router.get('/categoriegenderfilter/:gender', auth, async (req, res) => {
+	
+    let Categories = await Category.find({})
+  
+   
+    let CategoriesWithSectionsAndProducts =  await Promise.all(Categories.map((category)=> {
+           return {...category.toObject(), sectionsOfCategory: category.sectionsOfCategory.map((SOC)=>{
+    let sections=  await Section.findOne({_id: SOC.sectionOfCategory }, {gender : { $in: [req.params.gender, "all" ] }} )
+           return {...sections, productsOfSection: sections.productsOfSection.map((POS)=>{
+                    return await Product.findOne({_id: POS.productOfSection },  {gender : { $in: [req.params.gender, "all" ] }} )
+               })
+             })
+} )) 
+
+
+    try {
+ 
+        res.status(201).send(CategoriesWithSectionsAndProducts)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+
 module.exports = router
