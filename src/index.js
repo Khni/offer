21651c06beee76b72633883ef281eb862ce9778e
khaledeveloper <khaledeskandrany@gmail.com>
2020-09-03 02:09:21 +0200

    production

--- src/index.js
+++ src/index.js
@@ -104,12 +104,14 @@ app.use(SectionRouter)
 app.use(ProductRouter)
 app.use(CollectionRouter)
 
-// Right before your app.listen(), add this:
-app.use(express.static('client/build'));
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname + '/client/build/index.html'))
-  })
-
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static( 'client/build' ));
+ 
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
+    });
+}
+ 
 
 app.listen(port,()=>{
     console.log('ok')
