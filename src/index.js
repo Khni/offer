c6153d393b225a430eb845100062730232567c32
khaledeveloper <khaledeskandrany@gmail.com>
2020-09-03 02:14:05 +0200

    production heroku

--- src/index.js
+++ src/index.js
@@ -104,13 +104,13 @@ app.use(SectionRouter)
 app.use(ProductRouter)
 app.use(CollectionRouter)
 
-if (process.env.NODE_ENV === 'production') {
+//if (process.env.NODE_ENV === 'production') {
     app.use(express.static( 'client/build' ));
  
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
     });
-}
+//}
  
 
 app.listen(port,()=>{
