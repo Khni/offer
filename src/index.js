75bf730137ffdd7b31ff20845947013372aaee14
khaled <khaledeveloper@gmail.com>
2020-09-03 11:39:38 +0200

    path express

--- src/index.js
+++ src/index.js
@@ -105,7 +105,7 @@ app.use(ProductRouter)
 app.use(CollectionRouter)
 
 //if (process.env.NODE_ENV === 'production') {
-    app.use(express.static( '/client/build' ));
+    app.use(express.static(path_join(__dirname, '../client/build')));
  
     app.get('*', (req, res) => {
       //  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
