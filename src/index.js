a795c57275297a92687a4a25d5a50c76173078c2
khaled <khaledeveloper@gmail.com>
2020-09-03 11:06:20 +0200

    build

--- src/index.js
+++ src/index.js
@@ -105,11 +105,11 @@ app.use(ProductRouter)
 app.use(CollectionRouter)
 
 //if (process.env.NODE_ENV === 'production') {
-    app.use(express.static( '../client/build' ));
+    app.use(express.static( '/client/build' ));
  
     app.get('*', (req, res) => {
       //  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
-    res.sendFile(path.join(__dirname, '../client/public/index.html'));
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 //}
  
