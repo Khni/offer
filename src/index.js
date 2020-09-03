5e2fbddba387b1d7472959d7e24b72d087ef4048
khaled <khaledeveloper@gmail.com>
2020-09-03 10:38:01 +0200

    env

--- src/index.js
+++ src/index.js
@@ -104,14 +104,14 @@ app.use(SectionRouter)
 app.use(ProductRouter)
 app.use(CollectionRouter)
 
-if (process.env.NODE_ENV === 'production') {
+//if (process.env.NODE_ENV === 'production') {
     app.use(express.static( '../client/build' ));
  
-    app.get('/*', (req, res) => {
+    app.get('*', (req, res) => {
       //  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-}
+//}
  
 
 app.listen(port,()=>{
