9b08d5da497d2e5f33208f5a6c8fd93573491197
khaledeveloper <khaledeskandrany@gmail.com>
2020-09-05 23:39:50 +0200

    user passhash

--- src/models/User.js
+++ src/models/User.js
@@ -170,7 +170,7 @@ userSchema.pre('save', async function (next) {
     const user = this
 
     if (user.isModified('password')) {
-        user.password = await bcrypt.hash(user.password, 8)
+        user.local.password = await bcrypt.hash(user.local.password, 8)
     }
 
     next()
