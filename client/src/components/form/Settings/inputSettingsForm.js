a7bc6e1d646915b87b94e5fce7a59f22052a528a
khaledeveloper <khaledeskandrany@gmail.com>
2020-09-08 13:49:52 +0200

    formsettings

--- client/src/components/form/Settings/inputSettingsForm.js
+++ client/src/components/form/Settings/inputSettingsForm.js
@@ -12,7 +12,7 @@ export default class InputSettings extends Component {
          /* placeholder={ this.props.placeholder } */
           className="input-text-settings"
           type={ this.props.type }
-          value={ value }
+          value={ this.props.value}
           onChange={ onChange }
          required   
           oninvalid="this.setCustomValidity('The Field can not be Empty ')"
