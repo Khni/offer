6c130154dd7eee4360403a5e29b72ab65350f805
khaledeveloper <khaledeskandrany@gmail.com>
2020-09-15 13:46:42 +0200

    picComponent

--- client/src/components/productPage/pic/pic.component.js
+++ client/src/components/productPage/pic/pic.component.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import Style from './pic.scss'
+
+import FBicon from "../../form/img/Facebookicon.png"
+import googleicon from "../../form/img/googleicon.png"
+
 const ProductPic = (props) => {
 
 
@@ -9,6 +13,12 @@ const ProductPic = (props) => {
 <img src={props.imgURL}  className="product-img"/>
 <div>
     <p><h2 className="share-this-product">SHARE THIS PRODUCT</h2></p>
+    <div className="share-icons-product">
+    <img className="fb-btn" src={FBicon}  />
+    <img className="google-btn" src={googleicon}  />
+
+    </div>
+    
     </div>
 </div>
 
