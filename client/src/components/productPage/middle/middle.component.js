9eda65d19fcb233f4d2ba3702530c6d0bf313b25
khaledeveloper <khaledeskandrany@gmail.com>
2020-09-15 13:59:27 +0200

    productPage

--- client/src/components/productPage/middle/middle.component.js
+++ client/src/components/productPage/middle/middle.component.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Style from './middle.scss'
 
-
+import FBicon from "../../form/img/Facebookicon.png"
+import googleicon from "../../form/img/googleicon.png"
 import { useHistory } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
@@ -17,6 +18,16 @@ const MiddleProduct = (props) => {
     <p className="product-page-price">Price: {" "+props.price+" "}EGP</p>
     <p className="brand-text">Brand: Fashion | Similar products from Fashion</p>
     <button type="submit" className="custum-btn-form middle-btn" onClick={() => props.addItem(props.item)} >ADD TO CART</button>
+
+    <div>
+    <p><h2 className="share-this-product">SHARE THIS PRODUCT</h2></p>
+    <div className="share-icons-product">
+    <img className="fb-btn" src={FBicon}  />
+    <img className="google-btn" src={googleicon}  />
+
+    </div>
+    
+    </div>
 </div>
 
     );
--- client/src/components/productPage/pic/pic.component.js
+++ client/src/components/productPage/pic/pic.component.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import Style from './pic.scss'
 
-import FBicon from "../../form/img/Facebookicon.png"
-import googleicon from "../../form/img/googleicon.png"
+
 
 const ProductPic = (props) => {
 
@@ -11,15 +10,7 @@ const ProductPic = (props) => {
 
 <div className="ProductPic">
 <img src={props.imgURL}  className="product-img"/>
-<div>
-    <p><h2 className="share-this-product">SHARE THIS PRODUCT</h2></p>
-    <div className="share-icons-product">
-    <img className="fb-btn" src={FBicon}  />
-    <img className="google-btn" src={googleicon}  />
 
-    </div>
-    
-    </div>
 </div>
 
 
