cb6988bfa71440682a5f19ed1e2fd0aa71f0f2d9
khaledeveloper <khaledeskandrany@gmail.com>
2020-09-15 10:23:10 +0200

    searchrender

--- client/src/components/searchbox/searchbox.component.js
+++ client/src/components/searchbox/searchbox.component.js
@@ -12,12 +12,12 @@ constructor(props){
     this.props.SearchChange()
   }
 
-
+render() {
     return (
 
         <input type="text"   className="input-text-searchbox"    placeholder="search..."      value={this.props.SearchVal} onChange={this.searchUpdate.bind(this)}   />
     );
-
+} 
 }
 
 export default Searchbox;
