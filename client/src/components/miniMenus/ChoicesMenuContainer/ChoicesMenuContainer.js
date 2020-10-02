6e6d4276f281e31149ada90d91ffe1205d1185d0
khaledeveloper <khaledeskandrany@gmail.com>
2020-10-02 18:32:41 +0200

    err headd

--- client/src/components/miniMenus/ChoicesMenuContainer/ChoicesMenuContainer.js
+++ client/src/components/miniMenus/ChoicesMenuContainer/ChoicesMenuContainer.js
@@ -1,10 +1,5 @@
 import React, { useState } from 'react';
-import { ReactComponent as BoltIcon } from '../icons/bolt.svg';
-import { ReactComponent as Useraccount } from '../icons/useraccount.svg';
-import { ReactComponent as Favorite } from '../icons/favorite.svg';
-import { ReactComponent as Orders } from '../icons/orders.svg';
-import { ReactComponent as View } from '../icons/view.svg';
-import { ReactComponent as Settings } from '../icons/setting.svg';
+
 import MenuIcontText from '../MenuIcontText/MenuIcontText' 
 const ChoicesMenuContainer =(props) =>{
 
