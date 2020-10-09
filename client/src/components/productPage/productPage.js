e4e202a3b3c0dc8f8ec107e41a9fe33de259e13a
khaledeveloper <khaledeskandrany@gmail.com>
2020-10-10 01:56:01 +0200

    err

--- client/src/components/productPage/productPage.js
+++ client/src/components/productPage/productPage.js
@@ -11,7 +11,7 @@ import Header from '../headd/header/header'
 import {addItem} from '../../store/actions/CartItemsAction';
 import StarRatings from 'react-star-ratings';
  import ReactDOM from 'react-dom';
-//import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
  
  
