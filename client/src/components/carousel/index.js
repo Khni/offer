76da7308d85c104387624f408c8f436e27962da9
khaledeveloper <khaledeskandrany@gmail.com>
2020-10-10 17:05:56 +0200

    err

--- client/src/components/carousel/index.js
+++ client/src/components/carousel/index.js
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Slider from './components/slider'
 
+
+
+
+const Carousel =(props) =>{
 const images = [
   'https://images.unsplash.com/photo-1449034446853-66c86144b0ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80',
   'https://images.unsplash.com/photo-1470341223622-1019832be824?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2288&q=80',
@@ -9,4 +13,11 @@ const images = [
   'https://images.unsplash.com/photo-1534161308652-fdfcf10f62c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2174&q=80'
 ]
 
-ReactDOM.render(<Slider slides={images} />, document.querySelector('.main'))
+return (
+
+<Slider slides={images} />
+);
+
+} 
+
+export default Carousel
