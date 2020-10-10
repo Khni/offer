b261d98070be12f48fb2a126129c7596f49f7881
khaledeveloper <khaledeskandrany@gmail.com>
2020-10-10 17:29:22 +0200

    arrow carousel

--- client/src/components/carousel/components/slider.js
+++ client/src/components/carousel/components/slider.js
@@ -4,6 +4,8 @@ import { css, jsx } from '@emotion/core'
 import SliderContent from './SliderContent'
 import Slide from './slide'
 
+import Arrow from './Arrow'
+
 /**
  * @function Slider
  */
@@ -11,12 +13,45 @@ const Slider = props => {
   const getWidth = () => window.innerWidth
 
   const [state, setState] = useState({
+    activeIndex: 0,
     translate: 0,
     transition: 0.45
   })
 
-  const { translate, transition } = state
+  const { translate, transition, activeIndex } = state
 
+  const nextSlide = () => {
+    if (activeIndex === props.slides.length - 1) {
+      return setState({
+        ...state,
+        translate: 0,
+        activeIndex: 0
+      })
+    }
+
+    setState({
+      ...state,
+      activeIndex: activeIndex + 1,
+      translate: (activeIndex + 1) * getWidth()
+    })
+  }
+
+  const prevSlide = () => {
+    if (activeIndex === 0) {
+      return setState({
+        ...state,
+        translate: (props.slides.length - 1) * getWidth(),
+        activeIndex: props.slides.length - 1
+      })
+    }
+
+    setState({
+      ...state,
+      activeIndex: activeIndex - 1,
+      translate: (activeIndex - 1) * getWidth()
+    })
+  }
+  
   return (
     <div css={SliderCSS}>
       <SliderContent
@@ -28,6 +63,9 @@ const Slider = props => {
           <Slide key={slide + i} content={slide} />
         ))}
       </SliderContent>
+
+      <Arrow direction="left" handleClick={prevSlide} />
+      <Arrow direction="right" handleClick={nextSlide} />
     </div>
   )
 }
