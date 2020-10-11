bbd7f7f8d9b42bab077e0f4d7697d9e72ea6d14e
khaledeveloper <khaledeskandrany@gmail.com>
2020-10-11 22:51:03 +0200

    productpage

--- client/src/components/productPage/middle/middle.component.js
+++ client/src/components/productPage/middle/middle.component.js
@@ -15,7 +15,7 @@ const MiddleProduct = (props) => {
     return (
 
 <div className="MiddleProduct">
-<h1 className="product-page-title">{props.name}</h1>
+<h4 className="product-page-title">{props.name}</h4>
     <p className="product-page-price">Price: {" "+props.price+" "}EGP</p>
     <p className="brand-text">Brand: Fashion | Similar products from Fashion</p>
     <button type="submit" className="custum-btn-form middle-btn" onClick={() => props.addItemToCartItem(props.item, props.cartItems)} >ADD TO CART</button>
