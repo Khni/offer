0df475ae078f931b5176515e29b3f49811cb761f
khaledeveloper <khaledeskandrany@gmail.com>
2020-10-17 23:45:22 +0200

    err api

--- client/src/store/actions/APIs.js
+++ client/src/store/actions/APIs.js
@@ -12,7 +12,7 @@ export const USER_GET_ORDERS = '/api/user-add-address'
 
 //admins
 export const ADMIN_POST_LOGIN = '/api/admin/login'
-export const ADMIN_GET_ORDERS = '/api/admin/orders/''
+export const ADMIN_GET_ORDERS = '/api/admin/orders/'
 export const ADMIN_POST_CATEGORY = '/api/category/add'
 export const USER_POST_SECTION = '/api/section/add'
 export const USER_POST_PRODUCT = '/api/product/add' 
@@ -21,9 +21,9 @@ export const USER_POST_COLLECTION = '/api/collection/add'
 //(collections, products, categories, sections) fetching
 
 export const GET_CATEGORIES_WITHALL = '/api/categoriesWithAll'
-export const GET_SECTIONS_WITH_PRODUCTS = '/api/sections-with-products''
+export const GET_SECTIONS_WITH_PRODUCTS = '/api/sections-with-products'
 export const GET_SECTIONS = '/api/sections'
-export const GET_COLLECTIONS = '/api/collections''
+export const GET_COLLECTIONS = '/api/collections'
 export const GET_PRODUCTIOS = '/api/products'
 export const GET_CATEGORIES = '/api/categories'
 
