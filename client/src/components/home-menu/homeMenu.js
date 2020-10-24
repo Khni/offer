c50775a89fb6fdff23c41128011762472a3cf052
khaled <khaledeveloper@gmail.com>
2020-10-24 20:08:25 +0200

    lang used

--- client/src/components/home-menu/homeMenu.js
+++ client/src/components/home-menu/homeMenu.js
@@ -34,6 +34,17 @@ class homeMenu extends Component {
    }
   
   async componentDidMount() {
+    let lang = window.navigator.languages ? window.navigator.languages[0] : null;
+    lang = lang || window.navigator.language || window.navigator.browserLanguage || window.navigator.userLanguage;
+
+let shortLang = lang;
+if (shortLang.indexOf('-') !== -1)
+    shortLang = shortLang.split('-')[0];
+
+if (shortLang.indexOf('_') !== -1)
+    shortLang = shortLang.split('_')[0];
+
+console.log("lang"+lang+ shortLang);
 await this.FetchSectionsFromServer()
   }
   async componentDidUpdate(prevProps, prevState) {
