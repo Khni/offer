c315a29813d0fca484086bf3faca007b5f836faf
khaled <khaledeveloper@gmail.com>
2020-10-27 18:03:18 +0200

    googleapis done

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -8,8 +8,8 @@ const GooglePlusTokenStrategy = require('passport-google-plus-token')
 const AuthPassport = require("./passport")
 const routerPromise = require('express-promise-router')();
 const UserController = require('../controllers/userController')
-const {HandelErrors} = require('./userUtils')
-const {ObjIndexToZero}= require('./usersFuncs')
+const { HandelErrors } = require('./userUtils')
+const { ObjIndexToZero } = require('./usersFuncs')
 const validator = require('validator')
 var geoip = require('geoip-lite');
 var google = require('googleapis').google;
@@ -24,20 +24,20 @@ router.post('/api/signup', async (req, res) => {
     const username = req.body.username
     const repassword = req.body.repassword
     if (!validator.isEmail(email)) {
-                    return res.status(403).json({
+        return res.status(403).json({
             error_en: 'Email is invalid',
-            error_ar:  'الايميل غير صحيح' 
+            error_ar: 'الايميل غير صحيح'
         });
-                }
-    
+    }
+
     if (!email || !password) {
-return res.status(403).json({
-            error_en: 'You must provide Email and password', 
-            error_ar: 'يجب أن تدخل بريد الكتروني ورقم سري' 
+        return res.status(403).json({
+            error_en: 'You must provide Email and password',
+            error_ar: 'يجب أن تدخل بريد الكتروني ورقم سري'
         });
-       } 
-    
-    
+    }
+
+
     let userjson = await User.findOne({ "local.email": email })
     if (userjson) {
         return res.status(403).json({
@@ -52,8 +52,8 @@ return res.status(403).json({
       return res.status(403).json({ error: 'Username is already in use'});
     }*/
 
-   
-   
+
+
 
     if (password !== repassword) {
         return res.status(403).json({
@@ -64,27 +64,27 @@ return res.status(403).json({
 
 
 
-     userjson= await User.findOne( {
+    userjson = await User.findOne({
         $or: [
             { "google.email": email },
             { "facebook.email": email },
         ]
     });
-     
+
     if (userjson) {
-    	
+
         // merge them
         userjson.methods.push('local')
         userjson.local = {
-        email: email, 
-        password: password
-      }
+            email: email,
+            password: password
+        }
 
         try {
-           await userjson.save()
+            await userjson.save()
             const token = await userjson.generateAuthToken()
-           // res.status(201).send({ userjson, token })
-           res.send({ userjson, token })
+            // res.status(201).send({ userjson, token })
+            res.send({ userjson, token })
         } catch (e) {
             res.status(400).send(e.message)
         }
@@ -102,15 +102,15 @@ return res.status(403).json({
             local: {
                 email: email,
                 password: password,
-                
+
             }
             , ...req.body
         })
         try {
             await user.save()
             const token = await user.generateAuthToken()
-         //   res.status(201).send({ user, token })
-         res.send({ user, token })
+            //   res.status(201).send({ user, token })
+            res.send({ user, token })
         } catch (e) {
 
             res.status(400).send(HandelErrors(e.message))
@@ -146,71 +146,71 @@ router.get('/api/profile', auth, async (req, res) => {
 })
 
 //find user by id
-router.get('/api/user/:id',(req,res) => {
-    User.findById(req.params.id).then((user)=>{
-       res.send(user)
-    }).catch((e)=>{
-       res.status(500).send(e)
+router.get('/api/user/:id', (req, res) => {
+    User.findById(req.params.id).then((user) => {
+        res.send(user)
+    }).catch((e) => {
+        res.status(500).send(e)
     })
-  
+
 })
 
 
 //update user
-router.post('/api/user/update',auth, async(req,res) => {
-	const email = req.body.email
-	
-	if (!validator.isEmail(email)) {
-                    return res.status(403).json({
+router.post('/api/user/update', auth, async (req, res) => {
+    const email = req.body.email
+
+    if (!validator.isEmail(email)) {
+        return res.status(403).json({
             error_en: 'Email is invalid',
-            error_ar:  'الايميل غير صحيح' 
+            error_ar: 'الايميل غير صحيح'
         });
-                }
-	
-	
-	let user = await User.findOne({ "local.email": email })
-    if (user && req.user.local.email !== email ) {
+    }
+
+
+    let user = await User.findOne({ "local.email": email })
+    if (user && req.user.local.email !== email) {
         return res.status(403).json({
             error_en: 'Email is already in use',
             error_ar: 'البريد الالكترونى مسجل مسبقا '
         });
-        
+
     }
     let userphone = await User.findOne({ phone: req.body.phone })
-    console.log("ss"+userphone);
-    console.log("req"+req.body.phone);
-    if (userphone && req.user.phone !== req.body.phone ) {
+    console.log("ss" + userphone);
+    console.log("req" + req.body.phone);
+    if (userphone && req.user.phone !== req.body.phone) {
         return res.status(403).json({
             error_en: 'Phone is already in use',
-            error_ar:  'الهاتف مسجل مسبقا لمستخدم اخر' 
+            error_ar: 'الهاتف مسجل مسبقا لمستخدم اخر'
         });
-        
+
     }
-	
-	const filter = { _id: req.user._id};
-const update = { "local.email": req.body.email, name:req.body.name, phone:req.body.phone};
+
+    const filter = { _id: req.user._id };
+    const update = { "local.email": req.body.email, name: req.body.name, phone: req.body.phone };
 
 
 
 
-try {
+    try {
         let user = await User.findOneAndUpdate(filter, update, {
- // returnOriginal: false
- new: true
-});
-const token = req.token
-        res.send({ user, token})
+            // returnOriginal: false
+            new: true
+        });
+        const token = req.token
+        res.send({ user, token })
         console.log("userphone" + user.phone);
     } catch (error) {
         //const userToLogin =await User.verifyLogin(req.body.email,req.body.password)
         res.status(403).json({
             error_en: 'Error, please try again later',
-        error_ar: 'خطأ ، برجاء المحاولة لاحقا'
+            error_ar: 'خطأ ، برجاء المحاولة لاحقا'
         });
     }
 
 
-  
+
 })
 
 //logout
@@ -346,37 +346,37 @@ router.post('/api/login', async (req, res) => {
      */
 })
 
-router.get('/api/user-addresses',auth , (req, res) => {
-    
-    
+router.get('/api/user-addresses', auth, (req, res) => {
+
+
 
     try {
         console.log("address");
         let user = req.user
         const addressesList = user.addresses
         const defaultAddress = user.defaultAddress
-       // let addressesList  = ObjIndexToZero(addresses,defaultAddress)
-        res.send({user,addressesList, defaultAddress});
-      
+        // let addressesList  = ObjIndexToZero(addresses,defaultAddress)
+        res.send({ user, addressesList, defaultAddress });
+
     } catch (error) {
         //const userToLogin =await User.verifyLogin(req.body.email,req.body.password)
         res.status(400).send(error)
     }
 
-	
-    
+
+
 })
 
 
-router.get('/api/lists/list' , async (req, res)=>{
+router.get('/api/lists/list', async (req, res) => {
 
     try {
         const users = await User.find({})
-        res.send({users});
+        res.send({ users });
     } catch (error) {
-        res.status(400).send({error})
+        res.status(400).send({ error })
     }
-   
+
 })
 
 router.post('/api/user-add-address', auth, async (req, res) => {
@@ -385,10 +385,10 @@ router.post('/api/user-add-address', auth, async (req, res) => {
 
 
     try {
-        
+
         let user = req.user
-        console.log("name:"+user.name);
-        user.addresses = user.addresses.concat( {
+        console.log("name:" + user.name);
+        user.addresses = user.addresses.concat({
             country: "Egypt",
             firstName: req.body.firstName,
             lastName: req.body.lastName,
@@ -397,36 +397,36 @@ router.post('/api/user-add-address', auth, async (req, res) => {
             street: req.body.street,
             floor: req.body.floor,
             apartment: req.body.apartment
-        
-        } 
+
+        }
         )
-            await user.save()
-            const addresses =user.addresses
-            //get last item in array
-            const address = user.addresses.slice(-1).pop()
-            user.defaultAddress = address
-            await user.save()
-            
-            
-            const defaultAddress = user.defaultAddress
-        let addressesList  = ObjIndexToZero(addresses,defaultAddress)
         await user.save()
-        res.send({user,defaultAddress, addressesList});
+        const addresses = user.addresses
+        //get last item in array
+        const address = user.addresses.slice(-1).pop()
+        user.defaultAddress = address
+        await user.save()
+
+
+        const defaultAddress = user.defaultAddress
+        let addressesList = ObjIndexToZero(addresses, defaultAddress)
+        await user.save()
+        res.send({ user, defaultAddress, addressesList });
     } catch (error) {
-        res.status(400).send({error});
+        res.status(400).send({ error });
     }
 
 
-//   await  User.findById(req.params.id).then(async(user) => {
-//         user.addresses = user.addresses.concat(
-//             {firstName: req.body.firstName}
-//             )
-//             await user.save()
-//         res.send({user});
-//     }).catch((e) => {
-//         res.status(400).send({e});
+    //   await  User.findById(req.params.id).then(async(user) => {
+    //         user.addresses = user.addresses.concat(
+    //             {firstName: req.body.firstName}
+    //             )
+    //             await user.save()
+    //         res.send({user});
+    //     }).catch((e) => {
+    //         res.status(400).send({e});
 
-//     })
+    //     })
 
 })
 
@@ -442,7 +442,7 @@ router.post('/api/user-add-defaultAddress', auth, async (req, res) => {
 
 
     try {
-        
+
         let user = req.user
         AddressFind = user.addresses.find((address) => address._id == req.body.id)
         if (!AddressFind) {
@@ -450,25 +450,25 @@ router.post('/api/user-add-defaultAddress', auth, async (req, res) => {
         }
 
 
-        console.log("name:"+user.name);
+        console.log("name:" + user.name);
         user.defaultAddress = AddressFind
-            await user.save()
-            const defaultAddress =user.defaultAddress
-            
-            const addresses =user.addresses
-            let addressesList  = ObjIndexToZero(addresses,AddressFind)
-            await user.save()
-        res.send({user,defaultAddress, addressesList});
+        await user.save()
+        const defaultAddress = user.defaultAddress
 
-        
+        const addresses = user.addresses
+        let addressesList = ObjIndexToZero(addresses, AddressFind)
+        await user.save()
+        res.send({ user, defaultAddress, addressesList });
+
+
     } catch (error) {
-        res.status(400).send({error});
+        res.status(400).send({ error });
     }
 })
 router.get('/api/admin/deleteuser/:id', (req, res) => {
-	
-	
-	
+
+
+
     User.deleteOne({ _id: req.params.id }).then((user) => {
         res.send('succefully ' + user.count + 'member has been removed');
     }).catch((e) => {
@@ -481,54 +481,61 @@ router.get('/api/admin/deleteuser/:id', (req, res) => {
 //get visitor ip
 
 router.get('/api/getip', (req, res) => {
-	
-	var ip = (req.headers['x-forwarded-for'] || '').split(',').pop().trim() || 
-         req.connection.remoteAddress || 
-         req.socket.remoteAddress || 
-         req.connection.socket.remoteAddress
 
-	
-         try {
-        
-           res.status(200).send({geo: geoip.lookup(ip)})
-            
-        } catch (error) {
-            res.status(400).send({error});
-        }
+    var ip = (req.headers['x-forwarded-for'] || '').split(',').pop().trim() ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress
+
+
+    try {
+
+        res.status(200).send({ geo: geoip.lookup(ip) })
+
+    } catch (error) {
+        res.status(400).send({ error });
+    }
 })
 //google
 router.post('/api/goauth', (req, res) => {
-	
-	/*var ip = (req.headers['x-forwarded-for'] || '').split(',').pop().trim() || 
+
+    /*var ip = (req.headers['x-forwarded-for'] || '').split(',').pop().trim() || 
          req.connection.remoteAddress || 
          req.socket.remoteAddress || 
          req.connection.socket.remoteAddress*/
-         
-         
-oauth2Client.setCredentials({access_token: req.body.access_Token});
-var oauth2 = google.oauth2({
-  auth: oauth2Client,
-  version: 'v2'
-});
-oauth2.userinfo.get(
-  function(err, res) {
-    if (err) {
-       console.log(err);
-    } else {
-       console.log(res);
+
+
+    oauth2Client.setCredentials({
+        access_token: req.body.access_token,
+        // clientID: "746252017489-f5c1v2vlrlhum6vrl2epec0t74qccbvi.apps.googleusercontent.com",
+        // clientSecret: 'FGf7UrLXHsGSmwugR52e2_NU',
+    });
+    var oauth2 = google.oauth2({
+
+        auth: oauth2Client,
+        version: 'v2'
+    });
+   
+
+
+
+
+    try {
+        oauth2.userinfo.get(
+            function (err, resProfile) {
+                if (err) {
+                    console.log(err);
+
+                } else {
+                    console.log(resProfile);
+                    res.status(200).send({ resProfile})
+                }
+            });
+       
+
+    } catch (error) {
+        res.status(400).send({ error });
     }
-});
-
-
-
-	
-         try {
-        
-           res.status(200).send({res: res})
-            
-        } catch (error) {
-            res.status(400).send({error});
-        }
 })
 
 
@@ -538,12 +545,12 @@ routerPromise.route('/api/user/oauth/google')
 routerPromise.route('/api/user/oauth/facebook')
     .post(passport.authenticate('facebookToken', { session: false }), UserController.facebookOAuth)
 
-    router.post('/api/auth/google',
+router.post('/api/auth/google',
     passport.authenticate('google', { scope: ['profile'] }));
-    router.post('/api/google/callback', 
+router.post('/api/google/callback',
     passport.authenticate('google', { failureRedirect: '/login' }),
-    function(req, res) {
-      // Successful authentication, redirect home.
-      res.redirect('/');
+    function (req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/');
     });
 module.exports = { router, routerPromise } 
