67861e6f3ff9f16f8569a6f2006666312a4272db
khaledeveloper <khaledeskandrany@gmail.com>
2020-11-03 12:05:55 +0200

    form input

---
+++ client/src/components/form/input.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import InputCss from './inputFrom.css';
+export default class CustomInput extends Component {
+  render() {
+    const { input: { value, onChange } } = this.props;
+    return (
+      <table className="juv-form-group">
+        <tr>
+        <label htmlFor={ this.props.id } className="LabelMove">{ /*this.props.label*/ }</label>
+        <input 
+          name={ this.props.name }
+          id={ this.props.id }
+         /* placeholder={ this.props.placeholder } */
+        /*  className={this.props.classN}*/
+        className="inputLabelMove" 
+          type={ this.props.type }
+          value={ value }
+          onChange={ onChange }
+          placeholder={this.props.placeholder}
+         required   
+          oninvalid="this.setCustomValidity('The Field can not be Empty ')"
+    oninput="this.setCustomValidity('')"  />
+    
+    </tr>
+      </table>
+    );
+  }
+}
