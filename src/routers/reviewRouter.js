fdc65af1d367b2b31347ad0f6d2dcadb3444ffe2
khaledeveloper <khaledeskandrany@gmail.com>
2020-11-04 14:08:56 +0200

    reviews

--- src/routers/reviewRouter.js
+++ src/routers/reviewRouter.js
@@ -35,17 +35,17 @@ router.get('/api/product-reviews/:id', auth, async (req, res) => {
     const reviews = await Review.find({ productID: req.params.id})
   
 
-    let products = await Product.find({})
+    let product = await Product.find({_id : req.params.id})
   
 let productsWreviews =await Promise.all( products.map(async(product)=>{
 const productReviews = await Review.find({ productID: product._id})
-let rev5 = product.productReviews.filter((rev)=>rev.rate==5).length 
-    let rev4 = product.productReviews.filter((rev)=>rev.rate==4).length * 0.8
-    let rev3 = product.productReviews.filter((rev)=>rev.rate==3).length * 0.6
-    let rev2 = product.productReviews.filter((rev)=>rev.rate==2).length * 0.4
-    let rev1 = product.productReviews.filter((rev)=>rev.rate==1).length * 0.2
+let rev5 = productReviews.filter((rev)=>rev.rate==5).length 
+    let rev4 = productReviews.filter((rev)=>rev.rate==4).length * 0.8
+    let rev3 = productReviews.filter((rev)=>rev.rate==3).length * 0.6
+    let rev2 = productReviews.filter((rev)=>rev.rate==2).length * 0.4
+    let rev1 = productReviews.filter((rev)=>rev.rate==1).length * 0.2
     
-    let revsCount = product.productReviews.length
+    let revsCount = productReviews.length
     let ratingCount = (rev5+rev4+rev3+rev2+rev1) 
 let rating=(ratingCount/revsCount) *5
 if(rating== null) {
@@ -65,6 +65,46 @@ return {...product.toObject(),  reviews : productReviews, rating: rating}
         res.status(400).send({ e })
     }
 })
+
+
+
+
+router.get('/api/productWithReviews:id', auth, async (req, res) => {
+    const reviews = await Review.find({ productID: req.params.id})
+  
+
+    let product = await Product.find({_id : req.params.id})
+    
+    
+  
+const productReviews = await Review.find({ productID: product._id})
+let rev5 = productReviews.filter((rev)=>rev.rate==5).length 
+    let rev4 = productReviews.filter((rev)=>rev.rate==4).length * 0.8
+    let rev3 = productReviews.filter((rev)=>rev.rate==3).length * 0.6
+    let rev2 = productReviews.filter((rev)=>rev.rate==2).length * 0.4
+    let rev1 = productReviews.filter((rev)=>rev.rate==1).length * 0.2
+    
+    let revsCount = productReviews.length
+    let ratingCount = (rev5+rev4+rev3+rev2+rev1) 
+let rating=(ratingCount/revsCount) *5
+if(rating== null) {
+	rating=0
+	} 
+
+
+
+
+return {...product.toObject(),   rating: rating}
+}))
+
+    try {
+
+        res.status(200).send({ favoriteProducts })
+    } catch (e) {
+        res.status(400).send({ e })
+    }
+})
+
 
 module.exports = router
 
