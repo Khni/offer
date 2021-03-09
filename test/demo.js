e8853cf5b055cfb195951e27dc541d6511c07665
khaledeveloper <khaledeskandrany@gmail.com>
2021-03-09 21:10:20 +0200

    test

--- test/demo.js
+++ test/demo.js
@@ -1,94 +1,55 @@
 
 
-const chai = require('chai');
-const expect = chai.expect;
-const chaiAsPromised = require('chai-as-promised')
-chai.use(chaiAsPromised);
-const sinon = require('sinon');
-const sinonChai = require('sinon-chai');
-chai.use(sinonChai);
-const rewire = require('rewire');
+exports.add = function (a, b) {
+    return a + b
+}
 
-var demo = rewire('./demo');
+exports.addCallback = function (a, b, callback) {
+    setTimeout(() => {
+        return callback(null, a + b);
+    }, 500);
+}
 
-describe('demo', ()=>{
-    context('add', ()=>{
-        it('should add two numbers', ()=>{
-            expect(demo.add(1,2)).to.equal(3);
-        })
-    })
+exports.addPromise = function (a, b) {
+    // return Promise.reject(new Error('fake'))
+    return Promise.resolve(a + b)
+}
 
-    context('callback add', ()=>{
-        it('should test the callback', (done)=>{
-            demo.addCallback(1,2, (err, result)=>{
-                expect(err).to.not.exist;
-                expect(result).to.equal(3);
-                done();
-            })
-        })
-    })
+//spy on log
+exports.foo = () => {
+    //some operation
+    console.log('console.log was called');
+    console.warn('console.warn was called');
 
-    context('test promise', ()=>{
-        it('should add with a promise cb', (done)=>{
-            demo.addPromise(1,2).then((result)=>{
-                expect(result).to.equal(3);
-                done();
-            }).catch((ex)=>{
-                console.log('caught error')
-                done(ex);
-            })
-        })
+    return;
+}
 
-        it('should test a promise with return', ()=>{
-            return demo.addPromise(1,2).then((result)=>{
-                expect(result).to.equal(3)
-            })
-        })
+//stub createfile
+exports.bar = async (fileName) => {
+    await exports.createFile(fileName);
+    let result = await callDB(fileName);
 
-        it('should test promise with async await', async ()=>{
-            let result = await demo.addPromise(1,2);
+    return result;
+}
 
-            expect(result).to.equal(3);
-        })
+exports.createFile = (fileName) => {
+    console.log('---in createFile')
+    //fake create file
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log('fake file created');
+            return Promise.resolve('done');
+        }, 100);
+    });
+}
 
-        it('should test promise with chai as promised', async ()=>{
-            await expect(demo.addPromise(1,2)).to.eventually.equal(3);
-        });
-    })
-
-    context('test doubles', ()=>{
-        it('should spy on log', ()=>{
-            let spy = sinon.spy(console, 'log');
-            demo.foo();
-
-            expect(spy.calledOnce).to.be.true;
-            expect(spy).to.have.been.calledOnce;
-            spy.restore();
-        })
-
-        it('should stub console.warn', ()=>{
-            let stub = sinon.stub(console, 'warn').callsFake(()=>{ console.log('message from stub') });
-
-            demo.foo();
-            expect(stub).to.have.been.calledOnce;
-            expect(stub).to.have.been.calledWith('console.warn was called');
-            stub.restore();
-        })
-    })
-
-    context('stub private functions', ()=>{
-        it('should stub createFile', async ()=>{
-            let createStub = sinon.stub(demo, 'createFile').resolves('create_stub');
-            let callStub = sinon.stub().resolves('calldb_stub');
-
-            demo.__set__('callDB', callStub);
-
-            let result = await demo.bar('test.txt');
-
-            expect(result).to.equal('calldb_stub');
-            expect(createStub).to.have.been.calledOnce;
-            expect(createStub).to.have.been.calledWith('test.txt');
-            expect(callStub).to.have.been.calledOnce;
-        })
-    })
-})
+function callDB(fileName) {
+    console.log('---in callDB')
+    //fake create file
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log('fake db call');
+            resolve('saved');
+        }, 100);
+    });
+}
