a3064143c173d45e373a834f8fbd0080e43431c4
khaled <khaledeveloper@gmail.com>
2021-03-16 22:44:55 +0200

    ee

--- test/utils/mongoose.js
+++ test/utils/mongoose.js
@@ -3,18 +3,29 @@ const expect = chai.expect;
 const Product = require('../../src/models/Product')
 
 describe('mongoose Product', () =>{
+    before(function() {
+   
 
+        // runs before all tests in this file regardless where this line is defined.
+    });
 
-    it('new Product',(done)=>{
+    it('new Product',async()=>{
 
         // const product = new Product()
 
         // expect(product).to.have.property('_d')
-        let MainProduct =  Product.findById('5f6fd12a8b6f6b001799242f')
+    //  let mProduct=   Product.findById('5f6fd12a8b6f6b001799242f').then((product)=>
+    //   product
+    //   done()
+    //   )
+    //    .catch((e)=>e)
+
+
+        let MainProduct = await Product.findById('5f6fd12a8b6f6b001799242f')
         expect(MainProduct).to.have.property('_id')
 
 
-        done()
+        
 
         
     })
