b0f33f07383e82daf75bd5f7a961bd9121154609
khaled <khaledeveloper@gmail.com>
2021-04-04 19:53:40 +0200

    cart products hooks

--- client/src/components/addToCartBtn/addToCartBtn.js
+++ client/src/components/addToCartBtn/addToCartBtn.js
@@ -1,22 +1,57 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { confirmAlert } from 'react-confirm-alert'; // Import
 import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
 import { withRouter } from 'react-router-dom';
+import * as actions from '../../store/actions';
+import { connect } from 'react-redux';
 
 import AdjustItemCount from './adjustItemCount'
-const addToCartBtn = (props) => {
+const AddToCartBtn = (props) => {
+
+    const [cartProductQty, updateCartProductQty] = useState(false)
+    const [ProductQty, setProductQty] = useState(null)
+    const [ProductCart, setProductCart] = useState(null)
+
+    useEffect(() => {
+        const fetching = async () => {
+            await props.fetchCart(props.cartItems, props.token, props.isAuthenticated)
+
+        }
+        fetching();
+    }, [props.ProductsOfCart])
+
+    useEffect(() => {
+
+        const product = props.ProductsOfCart.find(product => product._id == props.item._id)
+        if (product) {
+            setProductCart(product)
+            setProductQty(product.quantity)
+        } else {
+            setProductCart(null)
+            setProductQty(0)
+
+        }
 
 
-const btns = [
-    {
-        label: "نعم، أريد أتمام الشراء",
-        onClick:()=> props.history.push('/cart')
-    },
-    {
-        label: "لا ، أريد الاستمرار في التسوق",
-        onClick:() => {}
-    },
-]
+        console.log("ProductOfCart second" + JSON.stringify(props.ProductsOfCart));
+
+
+    }, [props.ProductsOfCart])
+
+
+
+
+
+    const btns = [
+        {
+            label: "نعم، أريد أتمام الشراء",
+            onClick: () => props.history.push('/cart')
+        },
+        {
+            label: "لا ، أريد الاستمرار في التسوق",
+            onClick: () => { }
+        },
+    ]
     const submit = () => {
         confirmAlert({
             title: 'تم إضاقة المنتج بنجاح الي سلة المشتريات ، هل تريد أتمام الطلب؟',
@@ -36,9 +71,9 @@ const btns = [
     };
 
     const addItem = () => {
-        console.log("token from btn" +props.token);
-        props.addItemToCartItem(props.item, props.cartItems, props.token , props.isAuthenticated)
-        props.showAlarmWindowAction(btns,"تم إضافة المنتج بنجاح الي عربى الشراء، هل تريد إتمام الشراء؟")
+        console.log("token from btn" + props.token);
+        props.addItemToCartItem(props.item, props.cartItems, props.token, props.isAuthenticated)
+        props.showAlarmWindowAction(btns, "تم إضافة المنتج بنجاح الي عربى الشراء، هل تريد إتمام الشراء؟")
         //  submit()
     }
 
@@ -54,16 +89,16 @@ const btns = [
 
 
         <div>
-            {!checkItem() ? <button type="submit" className="custum-btn-form"
+            {!ProductCart ? <button type="submit" className="custum-btn-form"
                 onClick={addItem}
             >ADD TO CART</button> : <AdjustItemCount
 
-                    cartItems={props.cartItems}
-                    cartProducts={props.cartProducts}
-                    isAuthenticated={props.isAuthenticated}
-                    token={props.token}
-                    item={props.item}
-                    itemQuantity={checkItem().quantity} />}
+                cartItems={props.cartItems}
+                cartProducts={props.cartProducts}
+                isAuthenticated={props.isAuthenticated}
+                token={props.token}
+                item={props.item}
+                itemQuantity={ProductQty} />}
 
 
 
@@ -73,4 +108,18 @@ const btns = [
 }
 
 
-export default withRouter(addToCartBtn) 
+
+const mapStateToProps = (state) => {
+    return {
+
+        RefreshToken: state.userAuth.authUser.refreshToken,
+        ProductsOfCart: state.cartProductsReducer.cartProducts,
+
+    }
+}
+
+
+
+
+
+export default withRouter(connect(mapStateToProps, actions)(AddToCartBtn))
--- client/src/components/addToCartBtn/adjustItemCount.js
+++ client/src/components/addToCartBtn/adjustItemCount.js
@@ -1,35 +1,34 @@
-import React, {Component, useEffect , useState } from "react"
+import React, { useEffect , useState } from "react"
 import { confirmAlert } from 'react-confirm-alert'; // Import
 import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
 import { withRouter } from 'react-router-dom';
 import * as actions from '../../store/actions';
 import { connect } from 'react-redux';
-class AdjustItemCount extends Component{
-  constructor(props) {
-    super(props);
+const AdjustItemCount = (props) => {
 
-    this.state = {
-      productQty : 0,
-      updateQty: false
-    }
+  const [cartProductQty, updateCartProductQty] =useState(false)
+  const [ProductQty, setProductQty] =useState(null)
 
+  useEffect(()=>{
+    const fetching =async() =>{
+      await props.fetchCart(props.cartItems,props.token,props.isAuthenticated)
+      console.log("removed"+JSON.stringify(props.cartItems));
+      }
+      fetching();
+},[])
 
-  }
+  useEffect(()=>{
+   // if (props.ProductsOfCart.length>0) {
+      const product = props.ProductsOfCart.find(product=> product._id == props.item._id)
+      console.log("qty"+ product);
+      if (product) {
+        setProductQty(product.quantity)
+      }
+      
+      console.log("ProductOfCart second" + props.ProductsOfCart);
+   // }
 
-  // const [cartProductQty, updateCartProductQty] =useState(false)
-  // const [ProductQty, setProductQty] =useState(0)
-
-  // useEffect(()=>{
-  //    (async () => {
-  //     await props.fetchCart(props.cartItems,props.token,props.isAuthenticated)
-  //     console.log("cart items local: "+JSON.stringify(props.cartItems));
-  //     console.log("ProductOfCart" + props.cartProducts);
-  //     const Qty = props.cartProducts.find(product=> product._id == props.item._id)
-  //     console.log("qty"+ Qty);
-  //     //setProductQty(Qty.quantity)
-  //   })();
-
-  // },[props.cartItems])
+  },[props.ProductsOfCart])
 
   // useEffect(() => {
 
@@ -50,71 +49,38 @@ class AdjustItemCount extends Component{
   
 
 //     (async () => {
-//       await actions.fetchCart(props.cartItems,this.props.token,this.props.isAuthenticated)
-//       console.log("cart items local: "+JSON.stringify(this.props.cartItems));
+//       await actions.fetchCart(props.cartItems,props.token,props.isAuthenticated)
+//       console.log("cart items local: "+JSON.stringify(props.cartItems));
 //     })();
-//     console.log("cart useeffect" + JSON.stringify(this.props.cartProducts) );
+//     console.log("cart useeffect" + JSON.stringify(props.cartProducts) );
 
 //     updateCartProductQty(false)
 //   }
 
 
 //   }, []);
-async componentDidMount () {
-  await this.props.fetchCart(this.props.cartItems,this.props.token,this.props.isAuthenticated)
-   console.log("cart items local: "+JSON.stringify(this.props.cartItems));
-   console.log("ProductOfCart" +JSON.stringify(this.props.ProductsOfCart) );
-   const Qty = this.props.ProductsOfCart.find(product=> product._id == this.props.item._id)
-   console.log("qty"+ Qty.quantity);
-   this.setState({productQty:Qty.quantity })
-}
 
-async componentDidUpdate () {
-  if (this.state.updateQty) {
-    await this.props.fetchCart(this.props.cartItems,this.props.token,this.props.isAuthenticated)
+
+
+const increaseProductQty =async () =>{
   
-   const Qty = this.props.ProductsOfCart.find(product=> product._id == this.props.item._id)
-   console.log("qty"+ Qty.quantity);
-   this.setState({productQty:Qty.quantity,updateQty: false })
-
-
-    
-  }
-}
-
-
-increaseProductQty =async () =>{
-  
-  console.log("cartProducts before"+ JSON.stringify(this.props.ProductsOfCart) );
- await this.props.addItemToCartItem(this.props.item, this.props.cartItems, this.props.token , this.props.isAuthenticated)
- this.setState({updateQty: true })
-  console.log("cartProducts after"+ JSON.stringify(this.props.ProductsOfCart) );
-
-}
-decreaseProductQty =async () =>{
-  
-  console.log("cartProducts before"+ JSON.stringify(this.props.ProductsOfCart) );
- await this.props.removeItemFromCartItem(this.props.item, this.props.cartItems, this.props.token , this.props.isAuthenticated)
- this.setState({updateQty: true })
-  console.log("cartProducts after"+ JSON.stringify(this.props.ProductsOfCart) );
+  console.log("cartProducts before"+ JSON.stringify(props.ProductsOfCart) );
+ await props.addItemToCartItem(props.item, props.cartItems, props.token , props.isAuthenticated)
+ 
+  console.log("cartProducts after"+ JSON.stringify(props.ProductsOfCart) );
 
 }
 
-getProductQty =async ()=>{
-  await actions.fetchCart(this.props.cartItems,this.props.token,this.props.isAuthenticated)
-  const Qty = this.props.ProductsOfCart.find(product=> product._id === this.props.item._id)
-  return Qty.quantity
-}
 
-render() {
 
     return (
 
         <div className="AdjustItemCountCountainer">
-             <button className="decreaseItemCount"onClick={async() =>await this.decreaseProductQty()} >-</button>
+             <button className="decreaseItemCount"onClick={async() =>{await props.removeItemFromCartItem(props.item, props.cartItems, props.token , props.isAuthenticated)
+            console.log("removed"+JSON.stringify(props.cartItems));} } >-</button>
             
-    <p className="itemCount">{this.state.productQty}</p>
-    <button className="increaseItemCount" onClick={async() => await this.increaseProductQty()}  >+</button>
+    <p className="itemCount">{ProductQty}</p>
+    <button className="increaseItemCount" onClick={async() => await increaseProductQty()}  >+</button>
 
         </div>
 
@@ -122,13 +88,15 @@ render() {
 
     );
 }
-}
 
 
 
-const mapDispatchToprops = dispatch => ({
+const mapDispatchToProps = dispatch => ({
+  // fetchCartProduct: (cartItems,token,isAuthenticated)=>dispatch( actions.fetchCart(cartItems,token,isAuthenticated)) ,
+
+
     // addItem: item => dispatch(actions.addItem(item)),
-     //addItemToCartItem: (item, items) => dispatch(actions.addItemToCartItem(item, items)),
+    //  addItemToCartItem: (item, items) => dispatch(actions.addItemToCartItem(item, items)),
     //  removeItemFromCartItem:(item, cartItems) => dispatch(actions.removeItemFromCartItem(item, cartItems)),
     
     //  addItemToCartItem:(item, cartItems) => dispatch(actions.addItemToCartItem(item, cartItems)),
@@ -138,9 +106,7 @@ const mapDispatchToprops = dispatch => ({
    const mapStateToProps = (state) => {
      return {
       
-      RefreshToken: state.userAuth.authUser.refreshToken,
-      token: state.userAuth.authUser.token,
-      isAuthenticated: state.userAuth.authUser.isAuthenticated,
+       RefreshToken: state.userAuth.authUser.refreshToken,
        ProductsOfCart: state.cartProductsReducer.cartProducts,
       
      }
