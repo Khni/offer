210d4e8c9623f96f725969aed6cad218195a6b9f
khaledeveloper <khaledeskandrany@gmail.com>
2021-04-11 20:01:30 +0200

    err

--- client/src/components/searchProducts/searchProducts.js
+++ client/src/components/searchProducts/searchProducts.js
@@ -3,7 +3,7 @@ import { withRouter } from 'react-router-dom';
 import * as actions from '../../store/actions';
 import { connect } from 'react-redux';
 // import Logger from '../../config/logger/logger'
-import AdjustItemCount from './adjustItemCount'
+
 const SearchProducts = (props) => {
 
     
