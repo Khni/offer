76fe690c0ff852249fad8e4cc42758584dc6b3e0
khaledeveloper <khaledeskandrany@gmail.com>
2021-05-03 15:31:04 +0200

    draft

--- draft/categories.js
+++ draft/categories.js
@@ -43,7 +43,7 @@ const categoriesDocs = [
 
 ]
 
-const Products = [
+const ProductsDocs = [
   {
     "_id": ObjectId("54fd7392742abeef6186a68e")
     , "name": "product1"
@@ -79,7 +79,7 @@ const Products = [
 
 //I want to make an object  to be like that:
 
-const allCat = [
+const allCategories = [
   {
 
     name: "electronics",
@@ -118,52 +118,138 @@ const allCat = [
 
   }
 ]
-// const AllCategories = {
-//    {
-//   "name" : "electronics",
-//     "categories" : {
-//     {
-//       "name" : "embedded",
-//         "categories": {},
-//       "products" : { { "name" : "product1" } }
-//     },
-//     {
-//       "name" : "controllers",
-//         "categories": { },
-//       "products" : { { "name" : "product2" } }
-//     },
-//     {
-//       "name" : "cases",
-//         "categories": {
-//           {
-//             "name" : "big",
-//               "products" : { { "name" : "product3" } }
-//           }, {
-//             "name" : "small",
-//               "products" : { { "name" : "product4" } }
-//         },
-
-//         "products" : { { "name" : "product4" } }
-//       }
-//     }
-
-//   }
 
 
 
 
 
-[
-  { _id: "Books", path: null },
-  { _id: "Programming", path: ",Books," },
-  { _id: "Databases", path: ",Books,Programming," },
-  { _id: "Languages", path: ",Books,Programming," },
-  { _id: "MongoDB", path: ",Books,Programming,Databases," },
-  { _id: "dbm", path: ",Books,Programming,Databases," },
-  { _id: "Java", path: ",Books,Programming,Languages," },
-  { _id: "C++", path: ",Books,Programming,Languages," },
-  { _id: "C", path: ",Books,Programming,Languages," },
-  { _id: "Functional", path: ",Books,Programming,Languages,Java," },
-  { _id: "Spring", path: ",Books,Programming,Languages,Java," },
-  { _id: "MicroServices", path: ",Books,Programming,Languages,Java," },
+
+
+
+
+
+Hello guys I wanted to implement Materialized Path Category Hierarchy to mongodb in nodejs application and if I have those 2 docs and I want to get array of all of them like a tree as I will explain
+
+
+    const categoriesDocs = [
+      {
+        "_id": ObjectId("54fd7392742abeef6186a68e")
+        , "name": "electronics"
+        , "parent": "/"
+        , "category": "/electronics"
+      },
+    
+    
+      {
+        "_id": ObjectId("54fd7392742abeef6186a68e")
+        , "name": "embedded"
+        , "parent": "/electronics"
+        , "category": "/electronics/embedded"
+      },
+      {
+        "_id": ObjectId("54fd7392742abeef6186a68e")
+        , "name": "controllers"
+        , "parent": "/electronics"
+        , "category": "/electronics/controllers"
+      },
+    
+      {
+        "_id": ObjectId("54fd7392742abeef6186a68e")
+        , "name": "cases"
+        , "parent": "/electronics"
+        , "category": "/electronics/cases"
+      },
+    
+      {
+        "_id": ObjectId("54fd7392742abeef6186a68e")
+        , "name": "big"
+        , "parent": "/electronics/cases"
+        , "category": "/electronics/cases/big"
+      },
+      {
+        "_id": ObjectId("54fd7392742abeef6186a68e")
+        , "name": "small"
+        , "parent": "/electronics/cases"
+        , "category": "/electronics/cases/small"
+      },
+    
+    
     ]
+    
+    const ProductsDocs = [
+      {
+        "_id": ObjectId("54fd7392742abeef6186a68e")
+        , "name": "product1"
+        , "cost": 125
+        , "currency": "USD"
+        , "categories": ["/electronics/embedded"]
+      },
+      {
+        "_id": ObjectId("54fd7392742abeef6186a68e")
+        , "name": "product2"
+        , "cost": 134
+        , "currency": "USD"
+        , "categories": ["/electronics/controllers"]
+      },
+      {
+        "_id": ObjectId("54fd7392742abeef6186a68e")
+        , "name": "product3"
+        , "cost": 133
+        , "currency": "USD"
+        , "categories": ["/electronics/cases/big"]
+      },
+    
+      {
+        "_id": ObjectId("54fd7392742abeef6186a68e")
+        , "name": "product4"
+        , "cost": 188
+        , "currency": "USD"
+        , "categories": ["/electronics/cases/small", "/electronics/cases"]
+      },
+    
+    ]
+
+
+I want to have output Array like that:
+
+    const allCategories = [
+      {
+    
+        name: "electronics",
+        categories: [
+          {
+            name: "embedded",
+            categories: [],
+            products: [{ name: "product1" }]
+          },
+          {
+            name: "controllers",
+            categories: [],
+            products: [{ name: "product2" }]
+          },
+          {
+            name: "cases",
+            categories: [
+              {
+                name: "big",
+                categories: [],
+                products: [{ name: "product3" }]
+              },
+              {
+                name: "small",
+                categories: [],
+                products: [{ name: "product4" }]
+              }
+            ],
+            products: [{ name: "product1" }]
+          },
+    
+        ]
+    
+        ,
+        products: [{ "name": "product4" }]
+    
+      }
+    ]
+
+How can I get this array in this array by mongoose queries? 
